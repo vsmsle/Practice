fb40d071647671ed1fca879dbe0d3f955750d073
@A.rabets <nn.rabets@gmail.com>
2023-09-11 16:46:01 +0300

    practice 2

---
+++ practice_2/pr_2.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Ожин из последних просмотренных фильмов?'),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Ожин из последних просмотренных фильмов?'),
+      d = prompt('На сколько оцените его?');
+
+
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+
+console.log(personaMovieDB);
