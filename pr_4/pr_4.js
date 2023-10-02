af0120b967a5dd2326664217368fd0bd5e681685
Anastasia <shalfeyfo@gmail.com>
2023-10-02 10:44:41 +0300

    add pr_4

---
+++ pr_4/pr_4.js
@@ -0,0 +1,31 @@
+"use strict"
+
+//циклы
+//while- пока что что-то выполняется
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;  
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         // continue;
+//     }
+//     console.log(i);
+// }
+
+//цикл в цикле
+
+for ( let i = 0; i < 3; i++) {
+    
+}
