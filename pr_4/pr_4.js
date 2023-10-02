d4b2bda91b5086a89ca54f8a225ac9616b204afd
Anastasia Rabets <shalfeyfo@gmail.com>
2023-10-02 17:32:20 +0300

    new

--- pr_4/pr_4.js
+++ pr_4/pr_4.js
@@ -16,16 +16,47 @@ let num = 50;
 // }
 // while (num < 55);
 
-// for (let i = 1; i < 10; i++) {
-//     if (i === 6) {
-//         // break;
-//         // continue;
-//     }
-//     console.log(i);
-// }
+for (let i = 5; i < 11; i++) {
+    // if (i === 6) {
+    //     // break;
+    //     // continue;
+    // }
+    console.log(i);
+}
 
 //цикл в цикле
 
 for ( let i = 0; i < 3; i++) {
-    
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+    for (let j = 0;  j < i; j++) {
+        result += '*'
+    }
+
+    result  += '\n'
+   
+}
+
+console.log(result);
+
+
+ first: for ( let i = 0; i < 3; i++) {
+    console.log(`first level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`second level: ${j}`);
+        for (let k = 0; k < 5; k++) {
+            if (k === 2) continue first;
+            console.log(`third level: ${k}`);
+    }
+    }
 }
