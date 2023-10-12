d48b27181068ef71685612977f98b38788cfe11d
Anastasia Rabets <shalfeyfo@gmail.com>
2023-10-12 12:04:34 +0300

    add new pr2 fix

--- practice_2/pr_2.js
+++ practice_2/pr_2.js
@@ -20,26 +20,25 @@ const personaMovieDB = {
 
 for (let i = 0; i < 2; i++) {
   const a = prompt("Один из последних просмотренных фильмов?"),
-        b = prompt("На сколько оцените его?");
+    b = prompt("На сколько оцените его?");
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personaMovieDB.movies[a] = b;
-        console.log('Done!');
-    } else {
-        console.log('Error!');
-        i--;
-    }
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personaMovieDB.movies[a] = b;
+    console.log("Done!");
+  } else {
+    console.log("Error!");
+    i--;
+  }
 }
 
 if (personaMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
+  console.log("Просмотрено довольно мало фильмов");
 } else if (personaMovieDB.count >= 10 && personaMovieDB.count < 30) {
-    console.log("Вы классический зритель");
+  console.log("Вы классический зритель");
 } else if (personaMovieDB.count >= 30) {
-    console.log("Вы киноман");
+  console.log("Вы киноман");
 } else {
-    console.log("Произошла ошибка");
-    console.log('Произошла ошибка');
+  console.log("Произошла ошибка");
 }
 
 console.log(personaMovieDB);
