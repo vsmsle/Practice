d8022dc5d83ceab9448608a22f4dff6f1e951b3c
Anastasia Rabets <shalfeyfo@gmail.com>
2023-10-24 15:59:03 +0300

    new fix1

--- pr_6/pr_6.js
+++ pr_6/pr_6.js
@@ -4,15 +4,15 @@
 
 const usdCurr = 28;
 const eurCurr = 32;
+const discount = 0.9;
 
-function convert(amount, curr) {    
-  console.log(curr * amount);
+function convert(amount, curr) {
+  return curr * amount;
 }
 // amount, curr -фргумент функции
 
-
-convert(500, usdCurr);
-convert(500, eurCurr);
+// convert(500, usdCurr);
+// convert(500, eurCurr);
 
 // 1. В функцию мы передаем 2 аргумента. Не важно, как из назвать.
 
@@ -21,3 +21,29 @@ convert(500, eurCurr);
 // 3. Когда мы вызываем функцию, мы в круглые скобки передаем аргументы для параметров. Т.е первым у нас идет при вызове функции аргумент 500. Вот он как раз подставляется вместо amount. Внутри движок присваивает в amount = 500
 
 // И такая операция выполняется в порядке очередности)
+
+//!!RETERN!!
+
+function promotion(result) {
+  console.log(result * discount);
+}
+// const res = convert(500, usdCurr);
+// promotion(res); - равнозначно с нижней строчкой
+
+promotion(convert(500, usdCurr));
+// convert(500, usdCurr);
+
+function test() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+    if (i === 3);
+  }
+  console.log("Done");
+}
+
+test();
+
+
+function doNothing() {};
+console.log(doNothing() === undefined);
+//пример того, что функция всегда что-то возвращает 
